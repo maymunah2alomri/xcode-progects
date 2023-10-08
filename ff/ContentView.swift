c6bd5737487c19a12ffab82254247b933c209a9e
maymunah2alomri <memo1278m@hotmail.com>
2023-10-08 15:10:04 +0300

    Initial Commit

---
+++ ff/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ff
+//
+//  Created by maymunah alomri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ff/ffApp.swift
@@ -0,0 +1,17 @@
+//
+//  ffApp.swift
+//  ff
+//
+//  Created by maymunah alomri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
